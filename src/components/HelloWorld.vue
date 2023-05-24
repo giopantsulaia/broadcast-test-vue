2928b91b7d39fcc6c8076bb752e053887f8a15bc
giopantsulaia <giorgipantsulaia@redberry.ge>
2023-05-24 20:00:41 +0400

    feat: login page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,43 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
+import axios from "../configs/axios/index";
+const name = ref("");
+const password = ref("");
 
-defineProps({
-  msg: String,
-})
-
-const count = ref(0)
+const submit = () => {
+  axios.post("login", {
+    name: name.value,
+    password: password.value,
+  });
+};
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div
+    class="w-full h-screen flex flex-col items-center justify-center border border-black"
+  >
+    <form class="w-1/5 mx-auto flex flex-col" @submit.prevent="submit">
+      <label for="name">Name</label>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        v-model="name"
+        class="border border-gray-400 rounded-md p-2 outline-none"
+      />
+      <label for="password" class="mt-6">Password</label>
+      <input
+        type="password"
+        name="password"
+        id="password"
+        v-model="password"
+        class="border border-gray-400 rounded-md p-2 outline-none"
+      />
+      <button type="submit" class="mt-10 w-fit mx-auto p-2 text-xl">
+        Log In
+      </button>
+    </form>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
+<style scoped></style>
